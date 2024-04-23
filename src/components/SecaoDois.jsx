c94aba73d417a9ed0c6ae428a988d30ae60e9e86
silvaPool <silva.friends22@gmail.com>
2024-04-23 18:35:23 -0300

    Esqueleto da secao 2

--- src/components/SecaoDois.jsx
+++ src/components/SecaoDois.jsx
@@ -1,9 +1,38 @@
+import { Box, Stack, Typography } from "@mui/material";
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Gamer from "../assets/images/pc.jpg";
+
 function SecaoDois() {
-    return (
-        <div>
-            Aqui fica a segunda seção
-        </div>
-    )
+  return (
+    <Box
+      sx={{
+        border: "1px solid green",
+        height: "200vh",
+        width: "80%",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Stack sx={{border: '1px solid red', marginBottom: '3rem', width: '85%', height: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+        <Box>
+            <Typography>
+            A Square lidera a revolução tecnológica, trazendo o futuro até você hoje
+            </Typography>
+        </Box>
+        <Box>
+            sdkjdjadjajdf
+        </Box>
+      </Stack>
+      <Box>dsfsdfsdfsdfsdf</Box>
+      <Box>dsfsdfsdfsdfsdf</Box>
+      <Box>dsfsdfsdfsdfsdf</Box>
+    </Box>
+  );
 }
 
 export default SecaoDois;
