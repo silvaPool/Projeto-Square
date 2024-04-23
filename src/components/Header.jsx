a670f5ae8ed9dfa420178003dc212dff5d7a9b44
silvaPool <silva.friends22@gmail.com>
2024-04-23 17:04:00 -0300

    Header e SecaoUm estilizados (1)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,15 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Logo from "../assets/images/logo.jpg";
+import Logo from "../assets/images/logosemfundo.png";
 
 function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
+      <AppBar position="static" sx={{ textAlign: "center", padding: '5px', background: '#0b1419' }}>
+        <Box>
           <img src={Logo} alt="Logo do site" width={"60px"} />
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography> */}
-          {/* <Button color="inherit">Login</Button> */}
-        </Toolbar>
+        </Box>
       </AppBar>
     </Box>
   );
--- src/components/SecaoUm.jsx
+++ src/components/SecaoUm.jsx
@@ -1,18 +1,46 @@
 import { Box, Stack, Typography } from "@mui/material";
+import PC from "../assets/images/pc.jpg";
 
 function SecaoUm() {
   return (
-    <Stack spacing={2} sx={{ border: "1px solid red", height: "100vh" }}>
+    <Stack
+      spacing={2}
+      sx={{
+      
+        height: "180vh",
+        textAlign: "center",
+        padding: "25px",
+      }}
+    >
       <Box>
-        <Typography>Explore o Futuro da Tecnologia com a Square</Typography>
-
-        <Typography>
-            Imagem aqui
+        <Typography
+          sx={{
+            fontSize: "35px",
+            fontFamily: "Cinzel",
+            lineHeight: "106px",
+            letterSpacing: "5.6px",
+          }}
+        >
+          Explore o Futuro da Tecnologia com a Square
         </Typography>
+        <Box sx={{marginBottom: '3rem'}}>
+          <img
+            src={PC}
+            alt="Imagem de um PC"
+            className="img"
+            width={"1300x"}
+            height={"700px"}
+          />
+        </Box>
       </Box>
 
       <Box>
-        Texto aqui
+        <Typography sx={{ fontSize: "25px", fontFamily: "Montserrat", lineHeight: '42px', letterSpacing: '0.721px' }}>
+          Descubra o que o futuro da tecnologia reserva para você com a Square.
+          Oferecemos uma ampla variedade de computadores e periféricos de ponta
+          para impulsionar a sua produtividade, criatividade e entretenimento.
+          Explore nossos produtos e esteja pronto para o amanhã!
+        </Typography>
       </Box>
     </Stack>
   );
