d54f97fdd12d10c89f907f154f246dc58c9077c7
silvaPool <silva.friends22@gmail.com>
2024-04-23 17:45:03 -0300

    logo alterada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Logo from "../assets/images/logosemfundo.png";
+import Logo from "../assets/images/logoalternativo.png";
 
 function Header() {
   return (
