6733a4bd55f96a29563a5333758a98542d53c8e1
silvaPool <silva.friends22@gmail.com>
2024-04-24 10:17:26 -0300

    Alterações no formulário

--- src/components/SecaoCinco.jsx
+++ src/components/SecaoCinco.jsx
@@ -1,29 +1,26 @@
+import { Box } from "@mui/material";
+
 function SecaoCinco() {
   return (
     <>
-      <div class="form-container">
-        <div class="form">
-          <span class="heading">Get in touch</span>
-          <input placeholder="Name" type="text" class="input" />
-          <input placeholder="Email" id="mail" type="email" class="input" />
-          <textarea
-            placeholder="Say Hello"
-            rows="10"
-            cols="30"
-            id="message"
-            name="message"
-            class="textarea"
-          ></textarea>
-          <div class="button-container">
-            <div class="send-button">Send</div>
-            <div class="reset-button-container">
-              <div id="reset-btn" class="reset-button">
-                Reset
+      <Box sx={{border: '1px solid red', width: '50vw', margin: '0 auto'}}>
+        <div class="form-container">
+          <div class="form">
+            <span class="heading">Pronto para levar seu setup para o próximo nível?</span>
+            <input placeholder="Name" type="text" class="input" />
+            <input placeholder="Email" id="mail" type="email" class="input" />
+            <input placeholder="Telefone" id="tel" type="text" class="input" />
+            <div class="button-container">
+              <div class="send-button">Send</div>
+              <div class="reset-button-container">
+                <div id="reset-btn" class="reset-button">
+                  Reset
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Box>
     </>
   );
 }
